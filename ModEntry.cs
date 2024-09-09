b1f4e571fba8081dd6ee06f49c1073a62737c808
rokugin <rokugin@gmail.com>
2024-09-09 07:20:28 -0700

    Rebase commit to separate repos and add licenses

---
+++ ModEntry.cs
@@ -0,0 +1,111 @@
+﻿using StardewModdingAPI;
+using HarmonyLib;
+using StardewValley;
+using System.Reflection.Emit;
+using StardewValley.Companions;
+using StardewValley.Objects.Trinkets;
+
+namespace CustomTrinketSounds {
+    internal class ModEntry : Mod {
+
+        public static ModConfig? Config;
+        public static IMonitor? SMonitor;
+
+        public override void Entry(IModHelper helper) {
+            Config = helper.ReadConfig<ModConfig>();
+            SMonitor = Monitor;
+            
+            var harmony = new Harmony(ModManifest.UniqueID);
+
+            harmony.Patch(
+                original: AccessTools.Method(typeof(FlyingCompanion), nameof(FlyingCompanion.Update)),
+                transpiler: new HarmonyMethod(typeof(ModEntry), nameof(FlyingCompanion_Update_Transpiler))
+            );
+            harmony.Patch(
+                original: AccessTools.Method(typeof(IceOrbTrinketEffect), nameof(IceOrbTrinketEffect.Update)),
+                transpiler: new HarmonyMethod(typeof(ModEntry), nameof(IceOrbTrinketEffect_Update_Transpiler))
+            );
+            harmony.Patch(
+                original: AccessTools.Method(typeof(HungryFrogCompanion), nameof(HungryFrogCompanion.Update)),
+                transpiler: new HarmonyMethod(typeof(ModEntry), nameof(HungryFrogCompanion_Update_Transpiler))
+            );
+            harmony.Patch(
+                original: AccessTools.Method(typeof(HungryFrogCompanion), nameof(HungryFrogCompanion.tongueReachedMonster)),
+                transpiler: new HarmonyMethod(typeof(ModEntry), nameof(HungryFrogCompanion_tongueReachedMonster_Transpiler))
+            );
+        }
+
+        static string GetParrotAudioCueName() {
+            string cueName = Game1.soundBank.Exists("parrotCompanion") ? "parrotCompanion" : "parrot_squawk";
+            if (Config!.Debug) SMonitor!.Log($"Playing audio cue: {cueName}.", LogLevel.Info);
+            return cueName;
+        }
+
+        static IEnumerable<CodeInstruction> FlyingCompanion_Update_Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator) {
+            var method = AccessTools.Method(typeof(ModEntry), nameof(GetParrotAudioCueName));
+
+            var matcher = new CodeMatcher(instructions, generator);
+
+            matcher.MatchStartForward(new CodeMatch(OpCodes.Ldstr, "parrot_squawk")).ThrowIfNotMatch("Couldn't find match for parrot audio name");
+
+            matcher.Set(OpCodes.Call, method);
+
+            return matcher.InstructionEnumeration();
+        }
+
+        static string GetIceOrbAudioCueName() {
+            string cueName = Game1.soundBank.Exists("iceball") ? "iceball" : "fireball";
+            if (Config!.Debug) SMonitor!.Log($"Playing audio cue: {cueName}.", LogLevel.Info);
+            return cueName;
+        }
+
+        static IEnumerable<CodeInstruction> IceOrbTrinketEffect_Update_Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator) {
+            var method = AccessTools.Method(typeof(ModEntry), nameof(GetIceOrbAudioCueName));
+
+            var matcher = new CodeMatcher(instructions, generator);
+
+            matcher.MatchStartForward(new CodeMatch(OpCodes.Ldstr, "fireball")).ThrowIfNotMatch("Couldn't find match for ice orb audio name");
+
+            matcher.Set(OpCodes.Call, method);
+
+            return matcher.InstructionEnumeration();
+        }
+
+        static string GetFrogAudioCueName() {
+            string cueName = Game1.soundBank.Exists("frogCompanion") ? "frogCompanion" : "croak";
+            if (Config!.Debug) SMonitor!.Log($"Playing audio cue: {cueName}.", LogLevel.Info);
+            return cueName;
+        }
+
+        static IEnumerable<CodeInstruction> HungryFrogCompanion_Update_Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator) {
+            var method = AccessTools.Method(typeof(ModEntry), nameof(GetFrogAudioCueName));
+
+            var matcher = new CodeMatcher(instructions, generator);
+
+            matcher.MatchStartForward(new CodeMatch(OpCodes.Ldstr, "croak")).ThrowIfNotMatch("Couldn't find match for croak audio name");
+
+            matcher.Set(OpCodes.Call, method);
+
+            return matcher.InstructionEnumeration();
+        }
+
+        static string GetTongueAudioCueName() {
+            string cueName = Game1.soundBank.Exists("tongueHit") ? "tongueHit" : "fishSlap";
+            if (Config!.Debug) SMonitor!.Log($"Playing audio cue: {cueName}.", LogLevel.Info);
+            return cueName;
+        }
+
+        static IEnumerable<CodeInstruction> HungryFrogCompanion_tongueReachedMonster_Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator) {
+            var method = AccessTools.Method(typeof(ModEntry), nameof(GetTongueAudioCueName));
+
+            var matcher = new CodeMatcher(instructions, generator);
+
+            matcher.MatchStartForward(new CodeMatch(OpCodes.Ldstr, "fishSlap")).ThrowIfNotMatch("Couldn't find match for tongue audio name");
+
+            matcher.Set(OpCodes.Call, method);
+
+            return matcher.InstructionEnumeration();
+        }
+
+    }
+}
